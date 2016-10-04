716ace4ad5035c5bb12be634063dd510803f2edb
Bradford Loesch <bradford.loesch@gmail.com>
2016-10-03 19:29:12 -0500

    fixing selection sort bugs

--- selectionsort.py
+++ selectionsort.py
@@ -2,11 +2,11 @@ def randomArr(length):
     import random
     arr = []
     for i in range (0, length):
-        arr.append(random.randint(0,10000))
+        arr.append(random.randint(0,100))
     return arr
 
-testArray = randomArr(100000)
-# print "Unsorted:", testArray
+testArray = randomArr(10)
+print "Unsorted:", testArray
 
 def selectionSort(arr):
     for i in range (0,len(arr)):
@@ -21,32 +21,40 @@ def selectionSort(arr):
 
 # MinMax selection sorting method
 def selectionSortMM(arr):
+    arr =  [62, 22, 64, 51, 10, 48, 36, 4, 41, 59]
     maxcount = 0
-    endpos = len(arr)-1
+    end = len(arr)-1
     for i in range (0,len(arr)/2):
         min = arr[i]
         mini = i
         max = arr[i]
         maxi = i
-        for j in range (i+1, endpos+1):
+        for j in range (i+1, len(arr)-i):
             if(arr[j] < min):
                 min = arr[j]
                 mini = j
             if(arr[j] > max):
                 max = arr[j]
                 maxi = j
-        (arr[i], arr[mini]) = (arr[mini], arr[i])
-        (arr[maxi], arr[endpos]) = (arr[endpos], arr[maxi])
-        endpos -= 1
+        end = len(arr)-i-1
+        print arr, i, mini, len(arr)-i-1, maxi
+        if (mini == len(arr)-i-1 and maxi == i):
+            (arr[i], arr[mini]) = (arr[mini], arr[i])
+        elif (maxi == i):
+            (arr[maxi], arr[end]) = (arr[end], arr[maxi])
+            (arr[i], arr[mini]) = (arr[mini], arr[i])
+        else:
+            (arr[i], arr[mini]) = (arr[mini], arr[i])
+            (arr[maxi], arr[end]) = (arr[end], arr[maxi])
     return arr
 
 import time
-start = time.time()
-print "Min sort:", selectionSort(testArray)
-end = time.time()
-print (end - start)
-start = time.time()
+# start = time.time()
+# print "Min sort:", selectionSort(testArray)
+# end = time.time()
+# print (end - start)
 
+start = time.time()
 print "Min and max sort:", selectionSortMM(testArray)
 end = time.time()
 print (end - start)
